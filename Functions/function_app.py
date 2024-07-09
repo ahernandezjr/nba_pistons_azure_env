8ef8829fa54f9f451cc1957108ee067f000e13b2
ahernandezjr <ahernandezjr0@gmail.com>
2024-07-09 00:01:33 -0500

    0.1-2024_07_09 - Init commit of function.

--- Functions/function_app.py
+++ Functions/function_app.py
@@ -1,12 +1,33 @@
 import logging
 import azure.functions as func
 
+# Creation packages
+import os
+import time
+import pandas as pd
+from basketball_reference_web_scraper import client
+
+# Processing packages
+import os
+import numpy as np
+import pandas as pd
+
+# Filtering packages
+##
+
+
+
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 0 0 * * *", arg_name="myTimer", run_on_startup=True,
+@app.schedule(schedule="0 0 0 1 * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def player_data_collection(myTimer: func.TimerRequest) -> None:
+    '''
+    This function is triggered by a timer. It will collect player data from the NBA API and store it in a database.
+    '''
+
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('Python timer trigger function executed.')
+    result = 1 + 1
+    logging.info(f'Python timer trigger function executed. Test output: {result}')
